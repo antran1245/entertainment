ed4c91905006dbcd82bb4540d9cb17e1140eb59f
antran1245 <antran1245@gmail.com>
2023-03-21 16:22:50 -0700

    fix up the styling for login

--- src/components/account/Login.tsx
+++ src/components/account/Login.tsx
@@ -7,7 +7,7 @@ export default function Login() {
       <input placeholder="Email address" type="text"/>
       <input placeholder="Password" type="text" />
       <button type="button">Login to your account</button>
-      <p>Don't have an account? <span>Sign Up</span></p>
+      <p>Don't have an account? &nbsp;<span>Sign Up</span></p>
     </form>
   )
 }

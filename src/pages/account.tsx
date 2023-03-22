b77ed7e4236f3adb173606d33e66511355b41c20
antran1245 <antran1245@gmail.com>
2023-03-21 17:37:02 -0700

    added some styles to the login and signup box

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -7,11 +7,11 @@ import Signup from "@/components/account/Signup";
 import styles from '@/styles/Account.module.css'
 
 export default function Account() {
-  const [switchSetting, setSwitchSetting] = useState<boolean>(false);
+  const [switchSetting, setSwitchSetting] = useState<boolean>(true);
   return(
     <>
       <Head>
-        <title>Account</title>
+        <title>{switchSetting? 'Login' : 'Sign Up'} Page</title>
       </Head>
       <main className={styles.accountContainer}>
         <Image alt="logo" src={logo} className={styles.logo}/>

bf8f944f005123400267b3fb69de4da34e542463
antran1245 <antran1245@gmail.com>
2023-03-21 23:42:54 -0700

    preparing for home page design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Account from './account'
 
 export default function Home() {
   return (
@@ -10,9 +9,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main data-theme-mode="dark">
-        <Account/>
-      </main>
     </>
   )
 }

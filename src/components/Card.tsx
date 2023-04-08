1061a70720222d7005439fb8e97927003f5f21b4
antran1245 <antran1245@gmail.com>
2023-04-08 01:02:59 -0700

    adding play button onto cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,18 +4,28 @@ import bookmarkEmpty from '/public/assets/icon-bookmark-empty.svg'
 import bookmarkFull from '/public/assets/icon-bookmark-full.svg'
 import movie from '/public/assets/icon-category-movie.svg'
 import tv from '/public/assets/icon-category-tv.svg'
+import play from '/public/assets/icon-play.svg'
 import styles from '@/styles/Gallery.module.css'
+import { useState } from 'react'
 
 interface CardProp {
   item : SingleEntry
 }
 
 export default function Card({ item } : CardProp) {
+  const [overlay, setOverlay] = useState<boolean>(false)
+
   return(
     <div className={styles.card}>
-      <div className={styles.imgContainer}>
-        <Image src={`${item.thumbnail.regular.small.slice(1) }`} alt={`${item.title}`} width={280} height={174}/>
+      <div className={styles.imgContainer} onMouseEnter={() => setOverlay(true)} onMouseLeave={() => setOverlay(false)}>
+        <Image src={`${item.thumbnail.regular.small.slice(1)}`} alt={`${item.title}`} width={280} height={174} />
         <span className={styles.bookmark}><Image src={bookmarkEmpty} alt="bookmark" /></span>
+        <div className={styles.playButton} style={{ display: overlay ? 'flex' : 'none' }}>
+          <div>
+            <Image src={play} alt='playButton' />
+            <p>Play</p>
+          </div>
+        </div>
       </div>
       <div className={styles.cardInfo}>
         <p>{`${item.year}`}</p>

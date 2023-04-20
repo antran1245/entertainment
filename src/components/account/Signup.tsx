88e83d51885af1e4b2d9c01e606af5396de8ad65
antran1245 <antran1245@gmail.com>
2023-04-20 15:42:44 -0700

    working on password validation for creating account

--- src/components/account/Signup.tsx
+++ src/components/account/Signup.tsx
@@ -7,7 +7,8 @@ interface SignupProps {
 
 export default function Signup({ setSwitch } : SignupProps) {
   const [error, setError] = useState<{ email: boolean, password: boolean, repeat: boolean }>({ email: false, password: false, repeat: false })
-
+  const [char, setChar] = useState<{ symbols: boolean, uppercase: boolean, lowercase: boolean, numbers: boolean }>({ symbols: false, uppercase: false, lowercase: false, numbers: false })
+  
   const submitSignUp = (event: React.SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault()
     const email = event.currentTarget.emailAddress.value
@@ -36,6 +37,10 @@ export default function Signup({ setSwitch } : SignupProps) {
     }
   }
 
+  const validatePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChar({ ...char, uppercase: /[A-Z]{ 1,}/.test(event.target.value), symbols: /[!@#$ %^&*]{ 1,}/.test(event.target.value), lowercase: /[a-z]{ 1,}/.test(event.target.value), numbers: /[0-9]{ 1,}/.test(event.target.value) })
+  }
+
   const validateEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!(/^[\w.+\-]+@gmail\.com$/.test(event.target.value))) {
       setError({ ...error, email: true })
@@ -48,6 +53,13 @@ export default function Signup({ setSwitch } : SignupProps) {
       <h1>Sign Up</h1>
       <input type="email" placeholder='example1234@gmail.com' id='emailAddress' required className={error.email ? styles.error : ''} onBlur={validateEmail} autoComplete='off'/>
       <input type="password" placeholder='Password' id='password' required className={error.password ? styles.error : ''} />
+      <label>Password required at least one</label>
+      <div className={styles.requirementGrid}>
+        <p className={`${char.symbols? styles.greenLight : styles.redLight}`}><span>&#183;</span>symbol - !@#$%^&*</p>
+        <p className={`${char.uppercase? styles.greenLight : styles.redLight}`}><span>&#183;</span>uppercase</p>
+        <p className={`${char.lowercase? styles.greenLight : styles.redLight}`}><span>&#183;</span>lowercase</p>
+        <p className={`${char.numbers? styles.greenLight : styles.redLight}`}><span>&#183;</span>number</p>
+      </div>
       <input type="password" placeholder='Repeat password' name='repeatPassword' required className={error.repeat ? styles.error : ''} />
       <button type='submit'>Create an account</button>
       <p>Already have an account? &nbsp;<span onClick={() => setSwitch(true)}>Login</span></p>

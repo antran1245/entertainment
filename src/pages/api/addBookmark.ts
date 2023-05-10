91189a92ecdceb6d889f16078f68da13f6e407d8
An Tran <antran1245@gmail.com>
2023-05-10 10:59:58 -0700

    many to many relation find or create bookmark

--- src/pages/api/addBookmark.ts
+++ src/pages/api/addBookmark.ts
@@ -5,23 +5,68 @@ type Data = {
     result: boolean;
 }
 
+async function findOrCreateBookmark(userId: number, showId: number) {
+    let bookmark = await prisma.bookmark.findFirst({
+        where: {
+            show: {
+                id: {
+                    in: [showId]
+                }
+            },
+            user: {
+                id: {
+                    in: [userId]
+                }
+            }
+        }
+    })
+    if(!bookmark) {
+        await prisma.bookmark.create({
+            data: {
+                userId,
+                showId
+            }
+        })
+    }
+}
+
+async function findOrCreateShow(id: number, title: string, year: number, category: string) {
+    let show = await prisma.show.findFirst({
+        where: {
+            title,
+            year,
+            category
+        }
+    })
+    if(!show) {
+        show = await prisma.show.create({
+            data: {
+                title,
+                category,
+                year,
+                bookmarks: {
+                    create: [
+                        {
+                            user: {
+                                connect: {
+                                    id
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        })
+    } else {
+        await findOrCreateBookmark(id, show.id)
+    }
+}
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
     const { id, title, year, category } = req.body
-    let resp = await prisma.bookmark.create({
-        data: {
-            title,
-            year,
-            category,
-            user: {
-                connect: {
-                    id: id
-                }
-            }
-        }
-    })
-    console.log(resp, id)
+    await findOrCreateShow(id, title, year, category)
     res.json({result: true})
 }

36fdf45ca10107eea97c9e745dbd67da46481b59
An Tran <antran1245@gmail.com>
2023-05-10 13:04:07 -0700

    populate the shows you bookmark

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -5,18 +5,23 @@ type Data = {
   result: boolean;
   id?: number;
   data?: UserInfo | null;
-  bookmarks?: BookmarkInfo
+  bookmarks?: Show
 }
+
 type UserInfo = {
   password: string;
   id: number;
   bookmarks: BookmarkInfo
 }
+
 type BookmarkInfo = {
-  id: Number;
-  title: String;
-  year: Number;
-  category: String;
+  showId: number
+}[]
+
+type Show = {
+  title: string,
+  year: number,
+  category: string
 }[]
 
 export default async function handler(
@@ -26,10 +31,23 @@ export default async function handler(
   const {email, password} = req.body
   const result: UserInfo | null = await prisma.user.findUnique({
     where: { email: email },
-    select: { id: true, password: true, bookmarks: true }
+      select: {
+        id: true,
+        password: true,
+        bookmarks: {
+          select: {
+            showId: true
+          }
+        }
+    }
+  })
+  const shows: Show | null = await prisma.show.findMany({
+    where: {
+      id: { in: result?.bookmarks.map(item => item.showId)}
+    }
   })
   if(result && result.password === password) {
-    res.json({ result: true, id: result.id, bookmarks: result.bookmarks })
+    res.json({ result: true, id: result.id, bookmarks: shows })
   } else {
     res.json({ result: false })
   }

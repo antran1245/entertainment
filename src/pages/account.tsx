d205570b2411cba6183651e0c643d2ba77d3acf3
An Tran <antran1245@gmail.com>
2023-05-15 18:01:56 -0700

    account page icon return to home

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -5,6 +5,7 @@ import logo from '/public/assets/logo.svg'
 import Login from "@/components/account/Login";
 import Signup from "@/components/account/Signup";
 import styles from '@/styles/Account.module.css'
+import Link from "next/link";
 
 export default function Account() {
   const [switchSetting, setSwitchSetting] = useState<boolean>(true);
@@ -17,7 +18,9 @@ export default function Account() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.accountContainer}>
-        <Image alt="logo" src={logo} className={styles.logo}/>
+        <Link href="/">
+          <Image alt="logo" src={logo} className={styles.logo}/>
+        </Link>
         {switchSetting? <Login setSwitch={setSwitchSetting}/> : <Signup setSwitch={setSwitchSetting}/>}
       </main>
     </>

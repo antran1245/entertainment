4e39994c185fa2fbd5e05f594b0103cc07692df6
An Tran <antran1245@gmail.com>
2023-05-23 14:03:44 -0700

    working on a sign out page

---
+++ src/components/account/Signout.tsx
@@ -0,0 +1,13 @@
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+import { UserContext } from "@/context/userContext";
+import { useFetch } from "@/hooks/useFetch";
+import styles from "@/styles/Account.module.css";
+
+
+export default function Login() {
+    
+    return (
+    <div></div>
+    );
+}

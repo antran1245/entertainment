79787d54fb3dfad46d2c1db7d194a348779c4bd1
An Tran <antran1245@gmail.com>
2023-05-24 13:14:26 -0700

    created a sign out page

--- src/components/account/Signout.tsx
+++ src/components/account/Signout.tsx
@@ -1,13 +1,24 @@
 import { useContext, useState } from "react";
-import { useRouter } from "next/router";
 import { UserContext } from "@/context/userContext";
-import { useFetch } from "@/hooks/useFetch";
 import styles from "@/styles/Account.module.css";
 
+interface SignoutProps {
+        setSwitch: React.Dispatch<React.SetStateAction<boolean>>
+    }
 
-export default function Login() {
-    
+export default function Signout({ setSwitch } : SignoutProps) {
+    const { user, setUser, setIsBookmarkArr } = useContext(UserContext)
+
+    const SignOut = () => {
+        localStorage.removeItem('SccfmPhDeV')
+        setUser({ email: null, id: null, bookmarks: {movies: [], tvSeries: []} })
+        setIsBookmarkArr([])
+    }
+
     return (
-    <div></div>
+    <div className={styles.formBox}>
+        <p>Currently Login as {user.email}</p>
+        <button type='button' onClick={SignOut}>Sign Out</button>
+    </div>
     );
 }
--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { UserContext } from "@/context/userContext";
 import Head from "next/head";
 import Image from "next/image";
 import logo from '/public/assets/logo.svg'
@@ -6,9 +7,11 @@ import Login from "@/components/account/Login";
 import Signup from "@/components/account/Signup";
 import styles from '@/styles/Account.module.css'
 import Link from "next/link";
+import Signout from "@/components/account/Signout";
 
 export default function Account() {
   const [switchSetting, setSwitchSetting] = useState<boolean>(true);
+  const { user } = useContext(UserContext)
   return(
     <>
       <Head>
@@ -21,7 +24,8 @@ export default function Account() {
         <Link href="/">
           <Image alt="logo" src={logo} className={styles.logo}/>
         </Link>
-        {switchSetting? <Login setSwitch={setSwitchSetting}/> : <Signup setSwitch={setSwitchSetting}/>}
+        {user.email? <Signout setSwitch={setSwitchSetting}/> :
+        switchSetting? <Login setSwitch={setSwitchSetting}/> : <Signup setSwitch={setSwitchSetting}/>}
       </main>
     </>
   )
